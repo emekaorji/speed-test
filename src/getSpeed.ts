19a55cbfec411c6485e9876a45e52c99ba6a9caa
Emeka Orji <emekapraiseo@gmail.com>
2023-11-30 13:23:17 +0100

    feat: change prettier configuration

--- src/getSpeed.ts
+++ src/getSpeed.ts
@@ -7,70 +7,70 @@ import puppeteer from 'puppeteer';
 import Observable from 'zen-observable';
 
 interface Result {
-    downloadSpeed: number;
-    uploadSpeed: number;
-    downloadUnit: string | undefined;
-    downloaded: number;
-    uploadUnit: string | undefined;
-    uploaded: number;
-    latency: number;
-    bufferBloat: number;
-    userLocation: string | undefined;
-    userIp: string | undefined;
-    isDone: boolean;
+	downloadSpeed: number;
+	uploadSpeed: number;
+	downloadUnit: string | undefined;
+	downloaded: number;
+	uploadUnit: string | undefined;
+	uploaded: number;
+	latency: number;
+	bufferBloat: number;
+	userLocation: string | undefined;
+	userIp: string | undefined;
+	isDone: boolean;
 }
 
 async function init(
-    browser: Browser,
-    page: Page,
-    observer: ZenObservable.SubscriptionObserver<Result>,
+	browser: Browser,
+	page: Page,
+	observer: ZenObservable.SubscriptionObserver<Result>,
 ) {
-    let previousResult;
+	let previousResult;
 
-    while (true) {
-        // eslint-disable-next-line no-await-in-loop
-        const result: Result = await page.evaluate(() => {
-            const $ = document.querySelector.bind(document);
+	while (true) {
+		// eslint-disable-next-line no-await-in-loop
+		const result: Result = await page.evaluate(() => {
+			const $ = document.querySelector.bind(document);
 
-            return {
-                downloadSpeed: Number($('#speed-value')?.textContent),
-                uploadSpeed: Number($('#upload-value')?.textContent),
-                downloadUnit: $('#speed-units')?.textContent?.trim(),
-                downloaded: Number($('#down-mb-value')?.textContent?.trim()),
-                uploadUnit: $('#upload-units')?.textContent?.trim(),
-                uploaded: Number($('#up-mb-value')?.textContent?.trim()),
-                latency: Number($('#latency-value')?.textContent?.trim()),
-                bufferBloat: Number($('#bufferbloat-value')?.textContent?.trim()),
-                userLocation: $('#user-location')?.textContent?.trim(),
-                userIp: $('#user-ip')?.textContent?.trim(),
-                isDone: Boolean($('#speed-value.succeeded') && $('#upload-value.succeeded')),
-            };
-        });
+			return {
+				downloadSpeed: Number($('#speed-value')?.textContent),
+				uploadSpeed: Number($('#upload-value')?.textContent),
+				downloadUnit: $('#speed-units')?.textContent?.trim(),
+				downloaded: Number($('#down-mb-value')?.textContent?.trim()),
+				uploadUnit: $('#upload-units')?.textContent?.trim(),
+				uploaded: Number($('#up-mb-value')?.textContent?.trim()),
+				latency: Number($('#latency-value')?.textContent?.trim()),
+				bufferBloat: Number($('#bufferbloat-value')?.textContent?.trim()),
+				userLocation: $('#user-location')?.textContent?.trim(),
+				userIp: $('#user-ip')?.textContent?.trim(),
+				isDone: Boolean($('#speed-value.succeeded') && $('#upload-value.succeeded')),
+			};
+		});
 
-        if (result.downloadSpeed > 0 && !isDeepStrictEqual(result, previousResult)) {
-            observer.next(result);
-        }
+		if (result.downloadSpeed > 0 && !isDeepStrictEqual(result, previousResult)) {
+			observer.next(result);
+		}
 
-        if (result.isDone || result.uploadSpeed) {
-            browser.close();
-            observer.complete();
-            return;
-        }
+		if (result.isDone || result.uploadSpeed) {
+			browser.close();
+			observer.complete();
+			return;
+		}
 
-        previousResult = result;
+		previousResult = result;
 
-        // eslint-disable-next-line no-await-in-loop
-        await delay(100);
-    }
+		// eslint-disable-next-line no-await-in-loop
+		await delay(100);
+	}
 }
 
 export const getSpeed = () =>
-    new Observable<Result>((observer) => {
-        // Wrapped in async IIFE as `new Observable` can't handle async function
-        (async () => {
-            const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-            const page = await browser.newPage();
-            await page.goto('https://fast.com');
-            await init(browser, page, observer);
-        })().catch(observer.error.bind(observer));
-    });
+	new Observable<Result>((observer) => {
+		// Wrapped in async IIFE as `new Observable` can't handle async function
+		(async () => {
+			const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+			const page = await browser.newPage();
+			await page.goto('https://fast.com');
+			await init(browser, page, observer);
+		})().catch(observer.error.bind(observer));
+	});
